2390d6a1cc643360098bea3c3156e5cb8310e748
Yulia Novakovskaya <yonovakovskaya@gmail.com>
2021-12-05 20:51:49 +0200

    Added main file for work, file with his styles & json file with contacts

---
+++ src/App.js
@@ -0,0 +1,74 @@
+import { Component } from "react";
+import { nanoid } from "nanoid";
+import styles from "./App.module.scss";
+
+import Container from "./components/Container";
+import ContactForm from "./components/ContactForm";
+import ContactList from "./components/ContactList";
+import contacts from "./contacts.json";
+import Filter from "./components/Filter";
+
+class App extends Component {
+  state = {
+    contacts,
+    filter: "",
+  };
+
+  addContact = ({ name, number }) => {
+    const contact = {
+      id: nanoid(),
+      name,
+      number,
+    };
+
+    const alreadyFind = this.state.contacts.find(
+      (contact) => contact.name === name
+    );
+
+    alreadyFind
+      ? alert(`${name} is already in contacts.`)
+      : this.setState(({ contacts }) => ({ contacts: [contact, ...contacts] }));
+  };
+
+  findContact = (event) => {
+    this.setState({ filter: event.target.value });
+  };
+
+  getFilterContacts = () => {
+    const { contacts, filter } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+
+    return contacts.filter((contact) =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  deleteContact = (contactId) => {
+    this.setState((prevState) => ({
+      contacts: prevState.contacts.filter(
+        (contact) => contact.id !== contactId
+      ),
+    }));
+  };
+
+  render() {
+    const { filter } = this.state;
+    const FilterContacts = this.getFilterContacts();
+
+    return (
+      <Container>
+        <h1 className={styles.Title}>Phonebook</h1>
+        <ContactForm onSubmit={this.addContact} />
+
+        <h2 className={styles.Title}>Contacts</h2>
+        <Filter value={filter} onFindContact={this.findContact} />
+        <ContactList
+          onFilteredContacts={FilterContacts}
+          onDeleteContact={this.deleteContact}
+        />
+      </Container>
+    );
+  }
+}
+
+export default App;
